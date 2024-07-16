7bd5570e903919d3d896d2e6c6c0e482e5c59140
Bethany Allen <bethany.allen@bsse.ethz.ch>
2024-07-16 14:55:08 +0200

    Start tidying time bin functions

--- R/time_bins.R
+++ R/time_bins.R
@@ -1,33 +1,47 @@
 #' Build equal length time bins
 #'
-#' 'build_bins()' makes a table of time bins and is called within the time_bins
-#' function only in the case where equal_bins are specified by the user.
+#' A function to create a table of time bins. The function is called within the
+#' `time_bins()` function only in the case where `equal_bins` are specified by
+#' the user. By default, 100 1My bins are generated.
 #'
-#' @param start Age at which the oldest time bin should start.
-#' @param end Age at which the youngest time bin should end.
-#' @param n Number of bins to be made.
-#' @returns Tibble containing the start, end and midpoint ages of time bins.
+#' @param start \code{numeric}. Numerical age at which the oldest time bin
+#'    should start. Default is `100`.
+#' @param end \code{numeric}. Numerical age at which the youngest time bin
+#'    should end. Default is `0`.
+#' @param n \code{numeric}. Number of time bins to be made. Default is `100`.
+#' @returns A `dataframe` containing the start, end, and midpoint ages of the
+#'    generated time bins.
+#'
 #' @examples
-#' build_bins(start=100, end=0, n=100)
-#' build_bins(start=10, end=5, n=20)
+#' # Generate 100 time bins between 100My and present
+#' example1 <- build_bins()
+#' # Generate 20 time bins between 10My and 5My
+#' example2 <- build_bins(start = 10, end = 5, n = 20)
 #' @export
-build_bins <- function (start, end, n = NULL) {
-  # convert number of bins to bin length
-  interval <- abs((start - end) / n)
+build_bins <- function (start = 100, end = 0, n = 100) {
 
-  if (start > end) {
-    interval <- -abs(interval)
+  # Handling errors
+  if (!is.numeric(start) || !is.numeric(end)) {
+    stop("`start` and/or `end` are not of numeric class")
   }
 
-  # vector of bin boundary times
-  bin_bound_seq <- seq(start, end, interval)
+  if (end >= start) {
+    stop(paste("Start value must be larger than end value"))
+  }
 
-  # offset start and end times by 1 to build bins of required length
-  bins <- tibble(
+  # Convert number of bins to bin length
+  interval <- abs((start - end) / n)
+
+  # Vector of bin boundary times
+  bin_bound_seq <- seq(from = start, to = end, by = -interval)
+
+  # Offset start and end times by 1 to build bins of required length
+  bins <- data.frame(
     start = bin_bound_seq[-length(bin_bound_seq)],
-    end   = bin_bound_seq[-1]
+    end = bin_bound_seq[-1]
   )
 
+  # Add midpoints
   bins$midpoint <- rowMeans(bins)
   return(bins)
 }
@@ -35,114 +49,210 @@ build_bins <- function (start, end, n = NULL) {
 
 #' Build time bins using geological stages
 #'
-#' 'build_stages()' makes a table of time bins and is called within the
-#' time_bins function only in the case where stages are specified by the user.
+#' A function to create a table of time bins. The function is called within the
+#' `time_bins()` function only in the case where `stages` are specified by the
+#' user. By default, Cenozoic stages are returned.
 #'
-#' @param begin Age at which the oldest time bin should start.
-#' @param finish Age at which the youngest time bin should end.
-#' @returns Table containing the start, end and midpoint ages of time bins.
+#' @param start \code{numeric}. Numerical age at which the oldest time bin
+#'    should start. Default is `66`.
+#' @param end \code{numeric}. Numerical age at which the youngest time bin
+#'    should end. Default is `0`.
+#' @returns A `dataframe` containing the start, end, and midpoint ages of the
+#'    generated time bins.
+#'
 #' @examples
-#' build_stages(start=66, end=0)
+#' # Generate time bins for the Cenozoic
+#' example1 <- build_stages()
+#' # Generate time bins for the Mesozoic
+#' example2 <- build_stages(start = 252, end = 66)
 #' @export
-build_stages <- function(begin, finish) {
+build_stages <- function(start = 66, end = 0) {
+
+  # Handling errors
+  if (!is.numeric(start) || !is.numeric(end)) {
+    stop("`start` and/or `end` are not of numeric class")
+  }
+
+  if (end >= start) {
+    stop(paste("Start value must be larger than end value"))
+  }
+
+  # Load inbuilt table of geological stages
   bins <- geo_bins
   bins <- bins[3:6]
-  bins <- bins[which(bins$start<=begin & bins$end>=finish), ]
+
+  # Trim to relevant stages
+  bins <- bins[which(bins$start <= start & bins$end >= end), ]
+
   return(bins)
 }
 
 
 #' Build time bins using geological epochs
 #'
-#' 'build_epochs()' makes a table of time bins and is called within the
-#' time_bins function only in the case where epochs are specified by the user.
+#' A function to create a table of time bins. The function is called within the
+#' `time_bins()` function only in the case where `epochs` are specified by the
+#' user. By default, Cenozoic epochs are returned.
 #'
-#' @param start Age at which the oldest time bin should start.
-#' @param end Age at which the youngest time bin should end.
-#' @returns Table containing the start, end and midpoint ages of time bins.
+#' @param start \code{numeric}. Numerical age at which the oldest time bin
+#'    should start. Default is `66`.
+#' @param end \code{numeric}. Numerical age at which the youngest time bin
+#'    should end. Default is `0`.
+#' @returns A `dataframe` containing the start, end, and midpoint ages of the
+#'    generated time bins.
+#'
+#' @importFrom tidyr separate
 #' @examples
-#' build_epochs(start=251.902, end=66)
+#' # Generate time bins for the Cenozoic
+#' example1 <- build_stages()
+#' # Generate time bins for the Mesozoic
+#' example2 <- build_stages(start = 252, end = 66)
 #' @export
-build_epochs <- function(begin, finish) {
-  bins <- data("geo_bins")
+build_epochs <- function(start = 66, end = 0) {
+
+  # Handling errors
+  if (!is.numeric(start) || !is.numeric(end)) {
+    stop("`start` and/or `end` are not of numeric class")
+  }
+
+  if (end >= start) {
+    stop(paste("Start value must be larger than end value"))
+  }
+
+  # Load inbuilt table of geological stages
+  bins <- geo_bins
+
+  # Combine 'system' and 'series' names
   bins$epoch <- paste(bins$sys, bins$series)
   bins <- bins[c(10, 4:6)]
-  bins <- bins[which(bins$start<=begin & bins$end>=finish), ]
+
+  # Trim to relevant epochs
+  bins <- bins[which(bins$start <= start & bins$end >= end), ]
+
+  # Consolidate epochs
   epoch_tables <- split(bins, f = bins$epoch)
   new_bins <- data.frame()
   for(i in 1:length(epoch_tables)){
     epoch <- unique(epoch_tables[[i]][,1])
     start <- max(epoch_tables[[i]][,2])
     end <- min(epoch_tables[[i]][,3])
-    midpoint <- (start-end)/2 + end
+    midpoint <- (start - end)/2 + end
     e <- cbind(epoch, start, end, midpoint)
     new_bins <- rbind(new_bins, e)
   }
   bins <- new_bins[order(as.numeric(new_bins$start), decreasing = TRUE), ]
   bins <- separate(bins, epoch, into = c("period", "epoch"), sep=" ")
+
   return(bins)
 }
 
 
-#' Custom time bins
+#' Build time bins using a custom vector
 #'
-#' 'customise_bins()' takes a vector of time bin boundary ages (assumed to be -)
-#' and makes a table of start, end and midpoint ages.
-#' @param custom_bins A negative vector of time bin boundary ages
-#' @returns A table of start, end and midpoint ages
+#' A function to create a table of time bins. The function takes a vector of
+#' time bin boundary ages (assumed to be millions of years, or years, in the
+#' past) and makes a table of start, end, and midpoint ages.
+#'
+#' @param custom_bins \code{numeric}. A vector of time bin boundary ages.
+#' @returns A `dataframe` containing the start, end, and midpoint ages of the
+#'    generated time bins.
+#'
 #' @examples
-#' customise_bins(custom_bins = -c(264.26, 224.54, 192.9))
+#' # Create vector of bin boundary ages
+#' bin_boundaries <- c(264.26, 224.54, 192.9)
+#' # Generate time bins
+#' example1 <- customise_bins(custom_bins = bin_boundaries)
 #' @export
-customise_bins <- function(custom_bins){
-  bins <- data.frame(start=-custom_bins[-length(custom_bins)], end=-custom_bins[-1])
+customise_bins <- function(custom_bins = NULL){
+
+  # Handling errors
+  if (!is.numeric(custom_bins)) {
+    stop("`custom_bins` vector is not of numeric class")
+  }
+
+  # Sort bin boundaries
+  custom_bins <- sort(custom_bins, decreasing = TRUE)
+
+  # Create bins
+  bins <- data.frame(
+    start = custom_bins[-length(custom_bins)],
+    end = custom_bins[-1])
+
+  # Calculate midpoints
   bins$midpoint <- rowMeans(bins)
+
   return(bins)
 }
 
 
 
-#' Adjust stages time bins
+#' Modify time bins
 #'
-#' 'adjust_stages()' takes a table of time bin start, end and midpoint ages and
-#' makes adjustments depending on settings provided.
-#' @param bins A table of time bin start, end and midpoint ages.
-#' @param finish Ending age of the youngest time bin.
-#' @param rmv_bins Named bins will be removed from the bins object.
-#' @param quat When TRUE bins in the Quaternary are merged into one time bin.
-#' @param holo When TRUE bins in the Holocene will be merged into one time bin.
-#' @param holo_ple When TRUE bins in the Holocene will be merged into one time
-#' bin along with the Upper Pleistocene.
-#' @returns A table of start, end and midpoint ages
+#' A function to modify a table of time bin start, end, and midpoint ages,
+#' depending on any settings provided.
+#'
+#' @param bins \code{dataframe}. A `dataframe` of time bin start, end, and
+#'    midpoint ages.
+#' @param finish \code{numeric}. The age at which the youngest time bin ends.
+#' @param rmv_bins \code{character}. The names of time bins to be removed from
+#'    the `dataframe`.
+#' @param quat \code{logical}. When `quat = TRUE`, bins in the Quaternary are
+#'    merged into one time bin. Default is `FALSE`.
+#' @param holo \code{logical}. When `holo = TRUE`, bins in the Holocene are
+#'    merged into one time bin. Default is `FALSE`.
+#' @param holo_ple \code{logical}. When `holo_ple = TRUE`, bins in the Holocene
+#'    are merged into one time bin along with the Upper Pleistocene. Default is
+#'    `FALSE`.
+#' @returns A `dataframe` containing the start, end, and midpoint ages of the
+#'    generated time bins.
+#'
 #' @examples
-#' adjust_stages(bins, finish=192.9 , rmv_bins="Sinemurian")
-#' adjust_stages(bins, finish=0, quat=T)
+#' # Remove Sinemurian from a set of Jurassic time bins
+#' example1 <- build_stages(start = 202, end = 145)
+#' example1 <- adjust_stages(example1, finish = 145, rmv_bins = "Sinemurian")
+#' # Merge Quaternary time bin
+#' example2 <- build_stages()
+#' example2 <- adjust_stages(example2, finish = 0, quat = TRUE)
 #' @export
-adjust_stages <- function(bins, finish, rmv_bins=NULL, quat=F, holo=F,
-                          holo_ple=F){
+adjust_stages <- function(bins = NULL, finish = NULL, rmv_bins = NULL,
+                          quat = FALSE, holo = FALSE, holo_ple = FALSE){
+
+  # Handling errors
+  if (is.data.frame(bins) == FALSE) {
+    stop("`bins` should be a dataframe.")
+  }
+
+  if (!is.numeric(finish)) {
+    stop("`finish` is not of numeric class")
+  }
+
   # Remove named stages
   if(!is.null(rmv_bins)){
-    bins <- bins[which(bins$stage!=rmv_bins), ]
+    bins <- bins[which(bins$stage != rmv_bins), ]
   }
 
   # Merge bins in the Quaternary to one bin
   if(quat == T & finish < 2.58){
-    n_q_bins <- length(which(bins$start<=2.58))
-    bins<-bins[1:((nrow(bins))-n_q_bins), ]
-    bins <- rbind(bins, c("Quaternary", 2.58, finish, midpoint=(2.58-finish)/2))
+    n_q_bins <- length(which(bins$start <= 2.58))
+    bins<-bins[1:((nrow(bins)) - n_q_bins), ]
+    bins <- rbind(bins, c("Quaternary", 2.58, finish,
+                          midpoint = (2.58 - finish) / 2))
   }
 
   # Merge bins in the Holocene to one bin
-  if(holo == T & finish<=0.0117){
-    n_holo_bins_merge <- length(which(bins$start<=0.0117))
-    bins<-bins[1:((nrow(bins))-n_holo_bins_merge), ]
-    bins <- rbind(bins, c("Holocene", 0.0117, finish, midpoint=(0.0117-finish)/2))
+  if(holo == T & finish <= 0.0117){
+    n_holo_bins_merge <- length(which(bins$start <= 0.0117))
+    bins<-bins[1:((nrow(bins)) - n_holo_bins_merge), ]
+    bins <- rbind(bins, c("Holocene", 0.0117, finish,
+                          midpoint = (0.0117 - finish) / 2))
   }
 
   # Merge bins in the Holocene and Upper Pleistocene to one bin
-  if(holo_ple == T & finish<=0.0117){
-    n_bins_merge <- length(which(bins$start<=0.129))
-    bins<-bins[1:((nrow(bins))-n_bins_merge), ]
-    bins <- rbind(bins, c("Upper", 0.129, finish, midpoint=(0.129-finish)/2))
+  if(holo_ple == T & finish <= 0.0117){
+    n_bins_merge <- length(which(bins$start <= 0.129))
+    bins<-bins[1:((nrow(bins)) - n_bins_merge), ]
+    bins <- rbind(bins, c("Upper", 0.129, finish,
+                          midpoint = (0.129 - finish) / 2))
   }
 
   return(bins)
@@ -269,8 +379,8 @@ div_bins <- function(bin_type, begin, finish, t_res=NULL, n_bins=NA,
 #' time_bins_all_opts(bin_type="stages", begin=66, finish=0, lr_hr_bins="low")
 #' time_bins_all_opts(bin_type="stages", begin=66, finish=0, lr_hr_bins="high")
 #' @export
-time_bins_all_opts <- function(bin_type, begin, finish, n_bins = 100, 
-                               custom_bins = NULL, lr_hr_bins = "", 
+time_bins_all_opts <- function(bin_type, begin, finish, n_bins = 100,
+                               custom_bins = NULL, lr_hr_bins = "",
                                remove_bins = NULL, use_q = FALSE,
                                merge_holo = FALSE, merge_holo_ple = FALSE){
   if(bin_type == "equal_bins"){
@@ -333,7 +443,7 @@ time_bins_all_opts <- function(bin_type, begin, finish, n_bins = 100,
 #' time_bins(bin_type="custom_bins", custom_bins = -c(10:0))
 #' @export
 time_bins <- function(bin_type, begin, finish, n_bins = 100, custom_bins = NULL,
-                      remove_bins = NULL, use_q = FALSE, merge_holo = FALSE, 
+                      remove_bins = NULL, use_q = FALSE, merge_holo = FALSE,
                       merge_holo_ple = FALSE){
   if(bin_type == "equal_bins"){
     bins <- build_bins(start = begin, end = finish, n = n_bins)
