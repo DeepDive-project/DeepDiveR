4382e658ac8fdae96663e77d930f57c122b076b9
Rebecca Cooper <75476712+rebeccabcooper@users.noreply.github.com>
2024-11-15 15:14:56 +0100

    add_model() function draft

---
+++ R/add_model.R
@@ -0,0 +1,55 @@
+#' Set the architecture of models to be trained in deepdive
+#'
+#' A function to assign the number of layers and nodes in the long short-term
+#' memory and dense layers of a deep neural network in DeepDive.
+#'
+#' @param config \code{character}. The name of the configuration object, created
+#'   using create_config()`, that will be edited.
+#' @param lstm_nodes \code{numeric}. A numerical 'vector' designating the number
+#'   of nodes and layers to be used in the long short-term memory architecture.
+#' @param dense_nodes \code{numeric}. A numerical 'vector' designating the 
+#'   number of nodes and layers to be used in the dense node architecture.
+#' @param model_name \code{string}. An identifier to destinguish between models. 
+#'
+#' @returns Adds attributes for a new model, specified with lstm_nodes and
+#' dense_nodes.
+#'
+#' @details This function can be used to train multiple models in the DeepDive
+#' framework. Without running the function, a default of two lstm layers 64, 32 
+#' and two fully-connected dense node layers 64, 32 will be used.
+#'
+#' @importFrom R6 is.R6
+#' @examples
+#' # Import internal dataset
+#' data(carnivora)
+#' # Generate vector describing time bin boundaries
+#' bins <- c(66, 23, 2.6, 0)
+#' # Create configuration object
+#' config <- create_config(name = "carnivora",
+#'                         data_file = "data/carnivora_deepdive_input.csv",
+#'                         bins = bins,
+#'                         n_areas = length(unique(carnivora$Area)))
+#' # Add model architectures to the config file
+#' add_model(config = config, lstm_nodes = c(128, 64), dense_nodes = c(64, 32), name = "1")
+#' @export
+add_model <- function(config = NULL, lstm_nodes = NULL, dense_nodes = NULL, 
+                      model_name = "") {
+  
+  # Handling errors
+  if (is.R6(config) == FALSE) {
+    stop("`config` should be a configuration file.")
+  }
+  
+  #### add handling of errors to make sure lstm_nodes and dense_nodes contain
+  #### at least one numeric entry, or that if it is a vector it goes largest
+  #### number to smallest number.
+  lstm_nodes <- as.numeric(lstm_nodes)
+  dense_nodes <- as.numeric(dense_nodes)
+  
+  lstm_parameter_name <- paste0("lstms_layers_model_", model_name)
+  config$data$model_training[[lstm_parameter_name]] <- lstm_nodes
+  
+  dense_parameter_name <- paste0("dense_layers_model_", model_name)
+  config$data$model_training[[dense_parameter_name]] <- dense_nodes
+  
+}
