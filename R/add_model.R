4f6a4ecd52f8fdd12e5aaca45b663e8cb97951ec
Rebecca Cooper <75476712+rebeccabcooper@users.noreply.github.com>
2024-11-19 16:23:22 +0100

    added tests
    Additional point to prevent overwriting parameters given the same model_name is drafted but not currently functioning as it should.

--- R/add_model.R
+++ R/add_model.R
@@ -40,16 +40,40 @@ add_model <- function(config = NULL, lstm_nodes = NULL, dense_nodes = NULL,
     stop("`config` should be a configuration file.")
   }
   
-  #### add handling of errors to make sure lstm_nodes and dense_nodes contain
-  #### at least one numeric entry, or that if it is a vector it goes largest
-  #### number to smallest number.
-  lstm_nodes <- as.numeric(lstm_nodes)
-  dense_nodes <- as.numeric(dense_nodes)
+  if (!is.numeric(dense_nodes)) {
+    stop("`dense_nodes` must be numeric.")
+  }
   
-  lstm_parameter_name <- paste0("lstms_layers_model_", model_name)
+  if (!is.numeric(lstm_nodes)) {
+    stop("`lstm_nodes` must be numeric.")
+  }
+  
+  
+  lstm_nodes <- sort(lstm_nodes, decreasing=TRUE)
+  dense_nodes <- sort(dense_nodes, decreasing=TRUE)
+  
+  lstm_parameter_name <- paste0("lstm_model_", model_name)
+  
+  dense_parameter_name <- paste0("dense_model_", model_name)
+  
+  
+  #### below is not working, was intended to check if a parameter name is
+  #### already present in the model training module and if so append 1 to the
+  #### end or something like this to prevent overwriting.
+  #### Should it be mandatory to provide a model_name so it doesn't name a param
+  #### e.g. lstm_model_
+  if (!is.null(config$data$model_training[lstm_parameter_name])){
+    print("`model_name` must be different from existing lstm entries to prevent overwriting, model name has had the value 1 appended.")
+    lstm_parameter_name <- paste0(lstm_parameter_name, 1)
+  }
+  
+  if (!is.null(config$data$model_training[dense_parameter_name])){
+    print("`model_name` must be different from existing dense entries to prevent overwriting, model name has had the value 1 appended.")
+    dense_parameter_name <- paste0(dense_parameter_name, 1)
+  }
+      
   config$data$model_training[[lstm_parameter_name]] <- lstm_nodes
   
-  dense_parameter_name <- paste0("dense_layers_model_", model_name)
   config$data$model_training[[dense_parameter_name]] <- dense_nodes
   
 }
