517742874af46f70ce89e474167408955586dd74
Bethany Allen <ballen@gfz.de>
2025-02-11 18:30:30 +0100

    Add editing errors dependent on parameter

--- R/edit_config.R
+++ R/edit_config.R
@@ -5,10 +5,9 @@
 #'   the values currently held by each parameter can be checked using the
 #'   notation `config$data$[module]$[parameter]`. Extensive checks ensure that
 #'   it is possible for the specific parameter to hold the desired value.
+#'
 #' @param config \code{character}. The name of the configuration object, created
 #'   using `create_config()`, that will be edited.
-#' @param module \code{character}. The name of the block of the configuration
-#'   object to which the parameter belongs.
 #' @param parameter \code{character}. The name of the parameter to edit in
 #'   the configuration file.
 #' @param value \code{}. The value you want to assign to the parameter. The type
@@ -19,7 +18,7 @@
 #'   `config$write("file_name.ini")`.
 #'
 #' @details A full list of parameters, and details about the values they can
-#'    take, can be viewed using `data(parameter_table)`. Parameters relating to
+#'    take, can be viewed using `data(parameters)`. Parameters relating to
 #'    geographic area inclusion in the simulations are better altered using
 #'    `regions_matrix()`.
 #'
@@ -40,23 +39,109 @@
 #'             parameter = "present_diversity", value = 313)
 #'
 #' @export
-edit_config <- function(config = NULL, module = NULL,
-                        parameter = NULL, value = NULL){
+edit_config <- function(config = NULL, parameter = NULL, value = NULL){
 
-  # Handling errors
+  # Load parameter table
+  data(parameters)
+
+  # Initial handling errors
   if (is.R6(config) == FALSE) {
     stop("`config` should be a configuration file.")
   }
 
-  if (module %in% names(config$data) == FALSE) {
-    stop("`module` name does not exist in configuration file.")
+  if (is.null(parameter)) {
+    stop("A parameter must be provided, view table using data(parameters).")
   }
 
-  if (parameter %in%
-      names(config$data[[which(names(config$data) == module)]]) == FALSE) {
-    stop("`parameter` name does not exist in given module in configuration
-         file.")
+  if (parameter %in% parameters$parameter == FALSE) {
+    stop("Parameter does not exist in configuration file.")
   }
+
+  # Parameter-specific handling errors
+  if (parameters$limits[which(parameters$parameter == parameter)] ==
+      "character") {
+    if (!is.character(value)) {
+      stop("For this parameter, provided value must be a character string.")
+    }
+  }
+
+  if (parameters$limits[which(parameters$parameter == parameter)] ==
+      "logical") {
+    if (!is.logical(value)) {
+      stop("For this parameter, provided value must be logical (TRUE/FALSE).")
+    }
+  }
+
+  if (parameters$limits[which(parameters$parameter == parameter)] ==
+      "number range") {
+    if (!is.numeric(value) || length(value != 2)) {
+      stop("For this parameter, provided value must be a numerical vector of
+            length 2, such as c(0,1).")
+    }
+  }
+
+  if (parameters$limits[which(parameters$parameter == parameter)] ==
+      "numerical string") {
+    if (!is.numeric(value) || length(value <= 1)) {
+      stop("For this parameter, provided value must be a numerical vector with
+            length greater than 1, such as c(0,10).")
+    }
+  }
+
+  if (parameters$limits[which(parameters$parameter == parameter)] ==
+      "positive decimal") {
+    if (!is.numeric(value) || value < 0 || value > 1) {
+      stop("For this parameter, provided value must be between 0 and 1.")
+    }
+  }
+
+  if (parameters$limits[which(parameters$parameter == parameter)] ==
+      "positive decimal range") {
+    if (!is.numeric(value) || length(value != 2) ||
+        value[1] < 0 || value[1] > 1 || value[2] < 0 || value[2] > 1) {
+      stop("For this parameter, provided value must be a numerical vector of
+            length 2, with both values between 0 and 1, such as c(0.1, 0.9).")
+    }
+  }
+
+  if (parameters$limits[which(parameters$parameter == parameter)] ==
+      "positive integer") {
+    if (!is.numeric(value) || value < 0 || round(value, 0) != value) {
+      stop("For this parameter, provided value must be a positive whole number,
+            such as 5.")
+    }
+  }
+
+  if (parameters$limits[which(parameters$parameter == parameter)] ==
+      "positive integer range") {
+    if (!is.numeric(value) || length(value != 2) ||
+        value[1] < 0 || value[2] < 0 ||
+        round(value[1], 0) != value[1] || round(value[2], 0) != value[2]) {
+      stop("For this parameter, provided value must be a numerical vector of
+            length 2, with both values being positive whole numbers, such as
+            c(1, 5).")
+    }
+  }
+
+  if (parameters$limits[which(parameters$parameter == parameter)] ==
+      "positive number") {
+    if (!is.numeric(value) || value < 0) {
+      stop("For this parameter, provided value must be a positive number,
+            such as 1.2.")
+    }
+  }
+
+  if (parameters$limits[which(parameters$parameter == parameter)] ==
+      "positive number range") {
+    if (!is.numeric(value) || length(value != 2) ||
+        value[1] < 0 || value [2] < 0) {
+      stop("For this parameter, provided value must be a numerical vector of
+            length 2, with both values being a positive number, such as
+            c(0.1, 1.5).")
+    }
+  }
+
+  # ADD CODE FOR PARAMETERS IN SEVERAL MODULES
 
   config$data[[module]][[parameter]] <- paste(value, collapse = " ")
 }
